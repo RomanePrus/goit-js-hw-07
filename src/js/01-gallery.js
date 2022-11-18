c53b55e25215cf0ba42a44f3d3f2dd4ce75f64fd
RomanPrus <skimunion@gmail.com>
2022-11-18 10:53:27 +0200

    hm 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgSource = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src="${imgSource}" width="1280" height="720">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeWithEsc);
+      },
+    },
+    {
+      onClose: () => {
+        window.removeEventListener("keydown", closeWithEsc);
+      },
+    }
+  );
+
+  function closeWithEsc(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+}
